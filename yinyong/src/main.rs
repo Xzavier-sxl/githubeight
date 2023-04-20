b4fad1b2bdf9a789fc0577f65e1dabcff4bb3aa0
wyybc <2938827362@qq.com>
2023-04-20 19:41:49 +0800

    yy commit

--- yinyong/src/main.rs
+++ yinyong/src/main.rs
@@ -1,27 +1,26 @@
-fn main(){
-	let reference = &4;
-	match reference {
-		&val => println!("{:?}",val),
-	}
+fn main(){	
+	let reference = &4;	
+	match reference {		
+		&val => println!("{:?}",val),	
+	}		
+	match *reference {		
+		val => println!("{:?}",val),	
+	}		
 	
-	match *reference {
-		val => println!("{:?}",val),
-	}
 	
-	let _not_a_reference = 3; //不是引用
+	let _not_a_reference = 3; 		
 	
-	let ref _is_a_reference = 3;
+	let ref _is_a_reference = 3;		
 	
-	let value = 5;
-	let mut mut_value = 6;
-	match value {
-		ref n => println!("{:?}",n),
-	}
-	
-	match mut_value {
-		ref mut m => {
-			*m += 10;
-			println!("{:?}",m);
-		}
+	let value = 5;	
+	let mut mut_value = 6;	
+	match value {		
+		ref n => println!("{:?}",n),	
+	}		
+	match mut_value {		
+		ref mut m => {			
+			*m += 10;			
+			println!("{:?}",m);		
+		}	
 	}
 }

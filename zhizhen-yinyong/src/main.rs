29f5a8749abaaf313c404c99ca5e7f5d19f491f9
wyybc <2938827362@qq.com>
2023-04-20 18:52:57 +0800

    zzyy commit

---
+++ zhizhen-yinyong/src/main.rs
@@ -0,0 +1,27 @@
+fn main(){
+	let reference = &4;
+	match reference {
+		&val => println!("{:?}",val),
+	}
+	
+	match *reference {
+		val => println!("{:?}",val),
+	}
+	
+	let _not_a_reference = 3; //不是引用
+	
+	let ref _is_a_reference = 3;
+	
+	let value = 5;
+	let mut mut_value = 6;
+	match value {
+		ref n => println!("{:?}",n),
+	}
+	
+	match mut_value {
+		ref mut m => {
+			*m += 10;
+			println!("{:?}",m);
+		}
+	}
+}
